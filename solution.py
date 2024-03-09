7e5f9ec9e39907e1d31d2d3e99f72952caee1b87
Naomi Mogi <nyabokenaomi001@gmail.com>
2024-03-09 09:02:42 +0300

    If-else syntax

---
+++ solution.py
@@ -0,0 +1,26 @@
+#ORIGINAL CODE:
+def check_alive(health):
+    if():
+        health < 0
+        return false
+    else:
+        return true
+
+#SOLUTION FOR THE CORRECTED CODE:
+def check_alive(health):
+    if(health > 0):
+        return True
+    else:
+        return False
+    
+#SOLUTION IN JS
+    """
+    function checkAlive(health) {
+  if (health > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+    """
